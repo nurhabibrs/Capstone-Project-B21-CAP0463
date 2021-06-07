e624809cf8ec3d640626c8966878c974be8a47db
amaripujo <79905245+amaripujo@users.noreply.github.com>
2021-06-07 20:27:49 +0700

    update main.py
    add face detection

--- ML/Deployment/main.py
+++ ML/Deployment/main.py
@@ -1,5 +1,7 @@
 import json
 import keras
+import cv2
+import numpy as np
 from keras.models import load_model
 from keras.preprocessing import image
 #import joblib
@@ -12,22 +14,38 @@ app = Flask(__name__)
 model_json = open('model/model.json','r').read()
 model = keras.models.model_from_json(model_json)
 model.load_weights('model/model.h5')
+CLASSIFIER_PATH = 'model/haarcascade_frontalface_alt2.xml'
 
 @app.route("/predict", methods=["POST"])
 
-def predict(img_dir):
-	i = image.load_img(img_dir, target_size=(180, 180))
-	i = image.img_to_array(i)
-	i = i.reshape(1, 180, 180, 3)
-	p = model.predict_classes(i)
-	if p[0] < 0.5:
-		return("Adicted")
-	else:
-		return("Not Adicted")
+def predict(dir):
+  bgr_img = cv2.imread(dir)
+  rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
+  gray_img = cv2.cvtColor(src=rgb_img, code=cv2.COLOR_RGB2GRAY)
+  detector = cv2.CascadeClassifier(CLASSIFIER_PATH)
+  detections = detector.detectMultiScale(image=gray_img)
+  faces = sorted(tuple(data) for data in list(detections))
+  faces = tuple(faces)
+
+  img = image.load_img(dir, target_size=(180,180))
+  x = image.img_to_array(img)
+  x = np.expand_dims(x, axis=0)
+  images = np.vstack([x])
+  classes = model.predict(images, batch_size=10)
+
+#  return classes[0]
+
+  if len(faces)!=0:
+    if classes[0]<0.5:
+      return("Adicted")
+    else:
+      return("Not adicted")
+  else:
+    return("No Face Detected")
 
 #	return dic[round(p[0])]
 
-print(predict("M.jpeg"))
+print(predict("4196309-01.jpg"))
 
 #if __name__ == '__main__':
 #	app.run(host='0.0.0.0', port=5000)
